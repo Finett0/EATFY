5b6fdad3d83b9a93fd800f8267445cca83a5aefb
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-12-07 16:17:39 -0300

    Update app.py

--- eatfy/app.py
+++ eatfy/app.py
@@ -12,19 +12,33 @@ def Finalizar_App():
     os.system('cls')
     print('Aplicativo Encerrado!\n')
 
+def opcao_invalida():
+    print('Opção Invalida!\n')
+    input('Aperte qualquer tecla para voltar ao inicio do programa: ')
+    main()
+
+
+#print(f'Você escolheu a opção {opcao_escolhida}')
+
 def opcao_escolhida():
-    opcao_escolhida = int(input('Digite a opcao escolhida '))
-    if opcao_escolhida == 1:
-        print('Cadastro de Restaurantes: ')
+    try:
+        opcao_escolhida = int(input('Digite a opcao escolhida '))
+        if opcao_escolhida == 1:
+            print('Cadastro de Restaurantes: ')
 
-    elif opcao_escolhida == 2:
-        print('Lista de Restaurantes: ')
-    elif opcao_escolhida == 3:
-        print('Restaurantes ativados')
-    else:
-        Finalizar_App()
+        elif opcao_escolhida == 2:
+            print('Lista de Restaurantes: ')
+        elif opcao_escolhida == 3:
+            print('Restaurantes ativados')
+        elif opcao_escolhida == 4:
+            Finalizar_App()
+        else:
+            opcao_invalida()
 
+    except:
+        opcao_invalida()
 def main():
+    os.system('cls')
     nome_app()
     opcoes()
     opcao_escolhida()
